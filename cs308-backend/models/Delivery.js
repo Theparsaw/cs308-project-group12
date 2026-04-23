0df1e2fb7c144154f493392c36281758db272e5d
Kiyan Salehniya <kiyan.salehniya@sabanciuniv.edu>
2026-04-23 15:29:01 +0300

    SCRUM-128 create delivery model and schema

---
+++ cs308-backend/models/Delivery.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const deliveryItemSchema = new mongoose.Schema(
+  {
+    productId: String,
+    name: String,
+    unitPrice: Number,
+    quantity: Number,
+  },
+  { _id: false }
+);
+
+const deliverySchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+
+    // snapshot of items
+    items: {
+      type: [deliveryItemSchema],
+      required: true,
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+
+    //  since you DON’T have address in Order
+    address: {
+      type: String,
+      default: "Not provided",
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "processing",
+        "shipped",
+        "out_for_delivery",
+        "delivered",
+        "cancelled",
+      ],
+      default: "processing",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Delivery", deliverySchema);
